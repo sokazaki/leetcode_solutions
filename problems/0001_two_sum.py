f7fa5774bec894bb6ab63672b063b809efc658be
sokazaki <okazakisoichiro@gmail.com>
2019-06-27 13:43:37 +0900

    Create 0001_two_sum.py

---
+++ problems/0001_two_sum.py
@@ -0,0 +1,26 @@
+# O(N) Solution with HashMap
+# (Another: O(N^2) Solution with Simple Brute-force loop)
+
+import unittest
+
+def twoSum(nums, target):
+    d = {}
+    for i, n in enumerate(nums):
+        m = target - n
+        if m in d:
+            return [d[m], i]
+        else:
+            d[n] = i
+
+class Test(unittest.TestCase):
+
+    def test_twoSum(self):
+        self.assertListEqual(twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertListEqual(twoSum([2, 7, 11, 15], 26), [2, 3])
+        self.assertListEqual(twoSum([2, 9], 11), [0, 1])
+        self.assertListEqual(twoSum([-1, 4, 9], 8), [0, 2])
+
+
+if __name__ == "__main__":
+
+    unittest.main()
