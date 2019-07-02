c967af3adaa91fec458e9c14149d22fb04c965b7
sokazaki <okazakisoichiro@gmail.com>
2019-07-02 14:18:42 +0900

    Update 0023_merge_k_sorted_list.py

--- problems/0023_merge_k_sorted_list.py
+++ problems/0023_merge_k_sorted_list.py
@@ -8,22 +8,18 @@ from queue import PriorityQueue
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        sorted_list_head = sorted_list_tail = ListNode(0)
-        
-        pq = PriorityQueue()
-        
-        def add_node_in_pq(idx, node):
-            if node:
-                pq.put((node.val, idx, node))
-        
-        for idx, node in enumerate(lists):
-            add_node_in_pq(idx, node)
-        
-        while not pq.empty():
-            _, idx, node = pq.get()
-            add_node_in_pq(idx, node.next)
-            node.next = None
-            sorted_list_tail.next = node
-            sorted_list_tail = sorted_list_tail.next
-        
-        return sorted_list_head.next
+     
+        q = PriorityQueue()
+        dummy = ListNode(None)
+
+        for node in lists:
+            cur = node
+            while cur is not None:
+                q.put(cur.val)
+                cur = cur.next
+        cur = dummy
+        while not q.empty():
+            cur.next = ListNode(q.get())
+            cur = cur.next
+      
+        return dummy.next
