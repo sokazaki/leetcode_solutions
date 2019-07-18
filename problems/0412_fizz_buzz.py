4347e40a1d51661f9e22e5cacee8614514d3cd39
sokazaki <okazakisoichiro@gmail.com>
2019-07-19 01:39:12 +0900

    Update 0412_fizz_buzz.py

--- problems/0412_fizz_buzz.py
+++ problems/0412_fizz_buzz.py
@@ -1,3 +1,7 @@
+# O(N) Solution with Simple Approach
+
+import unittest
+
 def fizzBuzz(n):
     res = []
     for i in range(1,n+1):
@@ -10,3 +14,14 @@ def fizzBuzz(n):
         else:
             res.append(str(i))
     return res
+
+class Test(unittest.TestCase):
+
+    def test_fizzBuzz(self):
+        self.assertListEqual(fizzBuzz(1), ["1"])
+        self.assertListEqual(fizzBuzz(7), ["1","2","Fizz","4","Buzz","Fizz","7"])
+        self.assertListEqual(fizzBuzz(16), ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz","16"])
+
+if __name__ == "__main__":
+
+    unittest.main()
