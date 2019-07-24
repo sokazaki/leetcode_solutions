ea473b7aa0699e430315e2aaafc739c3f899f4c0
sokazaki <okazakisoichiro@gmail.com>
2019-07-24 11:43:13 +0900

    Update 0007_reverse_integer.py

--- problems/0007_reverse_integer.py
+++ problems/0007_reverse_integer.py
@@ -10,9 +10,9 @@ def reverse(x):
     else:
         x = str(x)
 
-    x = int(x[::-1])
+    x = int(x[::-1]) * sign
 
-    return 0 if x > 2**31-1 else x * sign
+    return 0 if x > 2**31-1 or x < -2**31 else x
 
 class Test(unittest.TestCase):
 
@@ -21,6 +21,10 @@ class Test(unittest.TestCase):
         self.assertEqual(reverse(-123), -321)
         self.assertEqual(reverse(120), 21)
         self.assertEqual(reverse(-150), -51)
+        self.assertEqual(reverse(int(str(2**31-1)[::-1])), 2147483647)
+        self.assertEqual(reverse(int(str(2**31)[::-1])), 0)
+        self.assertEqual(reverse(int("-"+str(2**31)[::-1])), -2147483648)
+        self.assertEqual(reverse(int("-"+str(2**31+1)[::-1])), 0)
 
 if __name__ == "__main__":
 
