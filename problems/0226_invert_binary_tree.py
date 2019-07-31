f6fc0956661d18506a1686646050ae1760d1bbf2
sokazaki <okazakisoichiro@gmail.com>
2019-07-31 12:48:14 +0900

    Create 0226_invert_binary_tree.py

---
+++ problems/0226_invert_binary_tree.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def invertTree(self, root):
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                node.left, node.right = node.right, node.left
+                queue.append(node.left)
+                queue.append(node.right)
+        return root
