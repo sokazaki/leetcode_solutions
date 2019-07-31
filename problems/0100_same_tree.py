0a556b12bfbe6efe91ee65b8629f5b7aacc3ed07
sokazaki <okazakisoichiro@gmail.com>
2019-07-31 13:11:10 +0900

    Update 0100_same_tree.py

--- problems/0100_same_tree.py
+++ problems/0100_same_tree.py
@@ -26,3 +26,18 @@ class Solution_bfs:
         return True
         
  
+class Solution_dfs:
+    def isSameTree(self, p, q):
+        stack = [[p, q]]
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                stack.append([node1.right, node2.right])
+                stack.append([node1.left, node2.left])
+        return True
