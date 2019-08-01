3c8f6218a219f44b5b6dd733500f8321271e6e61
sokazaki <okazakisoichiro@gmail.com>
2019-08-01 16:56:38 +0900

    Update 0108_convert_sorted_array_to_binary_search_tree.py

--- problems/0108_convert_sorted_array_to_binary_search_tree.py
+++ problems/0108_convert_sorted_array_to_binary_search_tree.py
@@ -7,15 +7,16 @@
 #         self.left = None
 #         self.right = None
 
-def sortedArrayToBST(self, nums):
-    if not nums:
-        return None
+class Solution:
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
 
-    mid = len(nums) // 2
+        mid = len(nums) // 2
 
-    root = TreeNode(nums[mid])
+        root = TreeNode(nums[mid])
 
-    root.left = self.sortedArrayToBST(nums[:mid])
-    root.right = self.sortedArrayToBST(nums[mid+1:])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
 
-    return root
+        return root
