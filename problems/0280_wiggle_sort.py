1cb950bc353d020afac597539e0b09fec49d253d
sokazaki <okazakisoichiro@gmail.com>
2019-08-06 15:30:18 +0900

    Create 0280_wiggle_sort.py

---
+++ problems/0280_wiggle_sort.py
@@ -0,0 +1,25 @@
+# O(N) Solution with Greedy Sort Approach
+
+import unittest
+
+def wiggleSort(nums):
+
+    for i in range(len(nums)):
+        if i%2==0 and nums[i]>nums[i-1] or i%2==1 and nums[i]<=nums[i-1]:
+            nums[i], nums[i-1] = nums[i-1], nums[i]
+
+    return nums
+
+
+class Test(unittest.TestCase):
+
+    def test_wiggleSort(self):
+        self.assertListEqual(wiggleSort([3,5,2,1,6,4]), [3,5,1,6,2,4])
+        self.assertListEqual(wiggleSort([3,-5,2,-1,66,-4]), [-5,2,-4,66,-1,3])
+        self.assertListEqual(wiggleSort([1,2,2,1,2,1,1,1,1,2,2,2]), [1,2,1,2,1,2,1,1,1,2,2,2])
+        self.assertListEqual(wiggleSort([1,2,0,1,2,-1,0,1,-1,2,2,-2]), [-2,2,0,2,-1,1,0,1,-1,2,1,2])
+
+
+if __name__ == "__main__":
+
+    unittest.main()
