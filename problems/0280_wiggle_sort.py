7c6ea79555760a8d12c10cde2a47b1e8ad57fec0
sokazaki <okazakisoichiro@gmail.com>
2019-08-06 15:40:36 +0900

    Update 0280_wiggle_sort.py

--- problems/0280_wiggle_sort.py
+++ problems/0280_wiggle_sort.py
@@ -4,7 +4,7 @@ import unittest
 
 def wiggleSort(nums):
 
-    for i in range(len(nums)):
+    for i in range(1, len(nums)):
         if i%2==0 and nums[i]>nums[i-1] or i%2==1 and nums[i]<=nums[i-1]:
             nums[i], nums[i-1] = nums[i-1], nums[i]
 
@@ -15,9 +15,9 @@ class Test(unittest.TestCase):
 
     def test_wiggleSort(self):
         self.assertListEqual(wiggleSort([3,5,2,1,6,4]), [3,5,1,6,2,4])
-        self.assertListEqual(wiggleSort([3,-5,2,-1,66,-4]), [-5,2,-4,66,-1,3])
+        self.assertListEqual(wiggleSort([3,-5,2,-1,66,-4]), [-5,3,-1,66,-4,2])
         self.assertListEqual(wiggleSort([1,2,2,1,2,1,1,1,1,2,2,2]), [1,2,1,2,1,2,1,1,1,2,2,2])
-        self.assertListEqual(wiggleSort([1,2,0,1,2,-1,0,1,-1,2,2,-2]), [-2,2,0,2,-1,1,0,1,-1,2,1,2])
+        self.assertListEqual(wiggleSort([1,2,0,1,2,-1,0,1,-1,2,2,-2]), [1,2,0,2,-1,1,0,1,-1,2,-2,2])
 
 
 if __name__ == "__main__":
