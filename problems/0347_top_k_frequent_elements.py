d8576792d12814b7b1ab50dbbe2a5f0905069ce4
sokazaki <okazakisoichiro@gmail.com>
2019-08-08 09:15:07 +0900

    Create 0347_top_k_frequent_elements.py

---
+++ problems/0347_top_k_frequent_elements.py
@@ -0,0 +1,36 @@
+# O(NlogK) Solution with Heap
+
+import unittest
+import heapq
+import collections
+
+def topKFrequent(nums, k):
+
+    if not nums:
+        return []
+    res = []
+    count = collections.Counter(nums)
+    vlist = []
+    heapq.heapify(vlist)
+
+    for key, val in count.items():
+        heapq.heappush(vlist, (-val, key))
+
+    while k:
+        top = heapq.heappop(vlist)
+        res.append(top[1])
+        k -= 1
+
+    return res
+
+class Test(unittest.TestCase):
+
+    def test_topKFrequent(self):
+        self.assertCountEqual(topKFrequent([1,1,1,2,2,3], 2), [1,2])
+        self.assertCountEqual(topKFrequent([1,1,2,2,3,3,4,4,4,5,5,5], 2), [4,5])
+        self.assertCountEqual(topKFrequent([1], 1), [1])
+        self.assertCountEqual(topKFrequent([1], 0), [])
+
+if __name__ == "__main__":
+
+    unittest.main()
