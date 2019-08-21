a3958e724a2ddf8b7e8f31db6aeb6db6ed616b5d
sokazaki <okazakisoichiro@gmail.com>
2019-08-21 15:50:49 +0900

    Update 0125_valid_palindrome.py

--- problems/0125_valid_palindrome.py
+++ problems/0125_valid_palindrome.py
@@ -3,11 +3,10 @@
 import unittest
 
 def isPalindrome(s):
-    i = 0
-    j = len(s)-1
-    while (i<j):
+    i, j = 0, len(s)-1
+    while i < j:
         if s[i].lower()==s[j].lower():
-            i +=1
+            i+=1
             j-=1
             continue
         elif not s[i].isalnum():
