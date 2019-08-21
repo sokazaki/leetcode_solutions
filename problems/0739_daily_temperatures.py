fb969aff6378034bd593b0a26c3840a0b54fe967
sokazaki <okazakisoichiro@gmail.com>
2019-08-21 15:39:04 +0900

    Update 0739_daily_temperatures.py

--- problems/0739_daily_temperatures.py
+++ problems/0739_daily_temperatures.py
@@ -1,15 +1,15 @@
 import unittest
 
 def dailyTemperatures(T):
-    ans = [0] * len(T)
+    res = [0] * len(T)
     stack = []
     for i, t in enumerate(T):
         while stack and T[stack[-1]] < t:
             cur = stack.pop()
-            ans[cur] = i - cur
+            res[cur] = i - cur
         stack.append(i)
 
-    return ans
+    return res
 
 class Test(unittest.TestCase):
 
