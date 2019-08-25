92e0c0f57b90961fe7865686a5795a24ff9a7a5f
sokazaki <okazakisoichiro@gmail.com>
2019-08-26 03:04:57 +0900

    Update 0193_valid_phone_numbers.sh

--- problems/0193_valid_phone_numbers.sh
+++ problems/0193_valid_phone_numbers.sh
@@ -1,3 +1,3 @@
-awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
-grep '^\([0-9]\{3\}-\|([0-9]\{3\}) \)[0-9]\{3\}-[0-9]\{4\}$' file.txt
-sed -n -r '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/p' file.txt
+awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt # ERE(Extended Regular Expression)
+grep '^\([0-9]\{3\}-\|([0-9]\{3\}) \)[0-9]\{3\}-[0-9]\{4\}$' file.txt # BRE(Basic Regular Expression)
+sed -n -r '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/p' file.txt # ERE(Extended Regular Expression)
