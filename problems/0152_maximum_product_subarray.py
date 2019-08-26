0ca75181d37c32774685b105e60eabf716e217c4
sokazaki <okazakisoichiro@gmail.com>
2019-08-26 15:28:45 +0900

    Create 0152_maximum_product_subarray.py

---
+++ problems/0152_maximum_product_subarray.py
@@ -0,0 +1,33 @@
+# O(N) Time Complexity / O(1) Space Complexity Solution with Dynamic Programming
+
+import unittest
+
+def maxProduct(nums):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+
+    pos = neg = 0
+    res = nums[0]
+    for n in nums:
+        if n > 0:
+            pos, neg = max(n, n*pos), min(0, n*neg)
+        else:
+            pos, neg = max(0, n*neg), min(n, n*pos)
+        res = max(pos, res)
+
+    return res
+
+
+class Test(unittest.TestCase):
+
+    def test_maxProduct(self):
+        self.assertEqual(maxProduct([2,3,-2,4]), 6)
+        self.assertEqual(maxProduct([-2,0,-1]), 0)
+        self.assertEqual(maxProduct([-2,5,-1]), 10)
+        self.assertEqual(maxProduct([-2]), 4)
+
+if __name__ == "__main__":
+
+    unittest.main()
