689bb61330fe5e8111bf248c62ee62cb0f8d2db9
sokazaki <okazakisoichiro@gmail.com>
2019-10-02 01:34:40 +0900

    Create 0362_design_hit_counter.py

---
+++ problems/0362_design_hit_counter.py
@@ -0,0 +1,25 @@
+class HitCounter:
+
+    def __init__(self):
+        self.counter = [[0,i+1] for i in range(300)]
+
+    def hit(self, timestamp):
+        idx = (timestamp-1)%300
+        if self.counter[idx][1] == timestamp:
+            self.counter[idx][0] += 1
+        else:
+            self.counter[idx][0] = 1            
+            self.counter[idx][1] = timestamp            
+
+    def getHits(self, timestamp):
+        res = 0
+        for x in self.counter:
+            if timestamp-x[1] < 300:
+                res += x[0]
+        return res
+
+
+# Your HitCounter object will be instantiated and called as such:
+# obj = HitCounter()
+# obj.hit(timestamp)
+# param_2 = obj.getHits(timestamp)
