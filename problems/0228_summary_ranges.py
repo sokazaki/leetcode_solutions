41caeed8bc17ccfa1dc47807b10cf08b0c0c0544
sokazaki <okazakisoichiro@gmail.com>
2019-10-07 13:39:38 +0900

    Update 0228_summary_ranges.py

--- problems/0228_summary_ranges.py
+++ problems/0228_summary_ranges.py
@@ -9,7 +9,7 @@ class Solution:
             return []
 
         res, i, start = [], 0, 0
-        for i in range(len(nums)-1):
+        while i < len(nums)-1:
             if nums[i]+1 != nums[i+1]:
                 res.append(self.helper(nums[start], nums[i]))
                 start = i+1
