b94ed015df635eed64670f20055aa51c160685fc
sokazaki <okazakisoichiro@gmail.com>
2019-10-08 15:18:59 +0900

    Create 0703_kth_largest_element_in_a_stream.py

---
+++ problems/0703_kth_largest_element_in_a_stream.py
@@ -0,0 +1,21 @@
+# O(NlogK) Solution with Priority Queue
+
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+        self.heap = nums
+        heapq.heapify(self.heap)
+        self.k = k
+
+    def add(self, val):
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
