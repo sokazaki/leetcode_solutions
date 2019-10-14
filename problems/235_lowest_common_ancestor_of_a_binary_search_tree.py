c6dc5a4aa1577ac7b7d30551125af5940f2db5d8
sokazaki <okazakisoichiro@gmail.com>
2019-10-14 13:05:57 +0900

    Create 235_lowest_common_ancestor_of_a_binary_search_tree.py

---
+++ problems/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,18 @@
+# O(logN) Solution with Simple Approach
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        while root:
+            if p.val < root.val  and q.val < root.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
