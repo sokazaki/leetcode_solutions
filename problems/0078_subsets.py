2ea609b8f2ce32cfc3be94eee477246ba5634354
sokazaki <okazakisoichiro@gmail.com>
2019-10-18 13:05:34 +0900

    Update 0078_subsets.py

--- problems/0078_subsets.py
+++ problems/0078_subsets.py
@@ -32,19 +32,19 @@ class Test(unittest.TestCase):
     # for Bit Manipulation
     def test_subsets(self):
         self.assertCountEqual(subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEquall(subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
+        self.assertCountEquall(subsets([]), [[]])
+        self.assertCountEqual(subsets([3,1,2]), [[],[3],[1],[1,3],[2],[2,3],[1,2],[1,2,3]])
+        self.assertCountEqual(subsets([3,6,1,7]), [[],[3],[6],[3,6],[1],[1,3],[1,6],[1,3,6],[7],[3,7],[6,7],[3,6,7],[1,7],[1,3,7],[1,6,7],[1,3,6,7]])
+        self.assertCountEqual(subsets([1]), [[],[1]])
 
     # for Backtracking
     def test_subsets(self):
         ans = Solution()
         self.assertCountEqual(ans.subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(ans.subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(ans.subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(ans.subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
-        self.assertCountEqual(ans.subsets([1,2,3]), [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]])
+        self.assertCountEqual(ans.subsets([]), [[]])
+        self.assertCountEqual(ans.subsets([3,1,2]), [[],[3],[3,1],[3,1,2],[3,2],[1],[1,2],[2]])
+        self.assertCountEqual(ans.subsets([3,6,1,7]), [[],[3],[3,6],[3,6,1],[3,6,1,7],[3,6,7],[3,1],[3,1,7],[3,7],[6],[6,1],[6,1,7],[6,7],[1],[1,7],[7]])
+        self.assertCountEqual(ans.subsets([1]), [[],[1]])
 
 if __name__ == "__main__":
 
