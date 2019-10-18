daf928e5a1434b247cdd1cd1032d1c2f1637c228
sokazaki <okazakisoichiro@gmail.com>
2019-10-18 15:09:33 +0900

    Create 0300_longest_increasing_subsequence.py

---
+++ problems/0300_longest_increasing_subsequence.py
@@ -0,0 +1,30 @@
+# O(NlogN) Solution with Dynamic Programming + Binary Search
+
+import unittest
+import bisect
+
+def lengthOfLIS(nums):
+
+    if not nums:
+        return 0
+    dp = [nums[0]]
+    for i in range(len(nums)):
+        if nums[i] > dp[-1]:
+            dp.append(nums[i])
+        else:
+            dp[bisect.bisect_left(dp, nums[i])] = nums[i]
+
+    return len(dp)
+
+
+class Test(unittest.TestCase):
+
+    def test_lengthOfLI(self):
+        self.assertEqual(lengthOfLIS([10,9,2,5,3,7,101,18]), 4)
+        self.assertEqual(lengthOfLIS([]), 0)
+        self.assertEqual(lengthOfLIS([10,9,2,5,6,7,101,18]), 5)
+        self.assertEqual(lengthOfLIS([10,9,2,5,-3,7,101,18]), 4)
+
+if __name__ == "__main__":
+
+    unittest.main()
