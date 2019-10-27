7f33befb8f20ff9fb4dd568f35598d2a9a6a7eb3
sokazaki <okazakisoichiro@gmail.com>
2019-10-27 10:17:31 +0900

    Create 0447_number_of_boomerangs.py

---
+++ problems/0447_number_of_boomerangs.py
@@ -0,0 +1,31 @@
+# O(N) Solution with Two Pointers
+
+import unittest
+from collections import defaultdict
+
+def numberOfBoomerangs(points):
+    res = 0
+    for x1, y1 in points:
+        dic = defaultdict(int)
+        for x2, y2 in points:
+            d1 = x2-x1
+            d2 = y2-y1
+            dic[d1**2+d2**2] += 1
+        for k in dic:
+            res += dic[k] * (dic[k]-1)
+    return res
+
+
+class Test(unittest.TestCase):
+
+    def test_numberOfBoomerangs(self):
+        self.assertEqual(numberOfBoomerangs([[0,0],[1,0],[2,0]]), 2)
+        self.assertEqual(numberOfBoomerangs([[0,0],[1,0],[2,0],[3,0]]), 4)
+        self.assertEqual(numberOfBoomerangs([[0,0],[1,0],[2,0],[3,0],[4,0]]), 8)
+        self.assertEqual(numberOfBoomerangs([[0,0],[1,0],[2,0],[3,0],[4,0],[5,0]]), 12)
+        self.assertEqual(numberOfBoomerangs([[0,0],[1,0]]), 0)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
