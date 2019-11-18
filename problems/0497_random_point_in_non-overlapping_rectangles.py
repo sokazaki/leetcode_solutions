284e2be110899b7a81d66ff2232100426bdf61f6
sokazaki <okazakisoichiro@gmail.com>
2019-11-18 18:11:39 +0900

    Create 0497_random_point_in_non-overlapping_rectangles.py

---
+++ problems/0497_random_point_in_non-overlapping_rectangles.py
@@ -0,0 +1,18 @@
+import random
+
+class Solution:
+
+    def __init__(self, rects):
+        self.rects, self.ranges, sm = rects, [], 0
+        for x1, y1, x2, y2 in rects:
+            sm += (x2 - x1 + 1) * (y2 - y1 + 1)
+            self.ranges.append(sm)
+
+    def pick(self):
+        x1, y1, x2, y2 = self.rects[bisect.bisect_left(self.ranges, random.randint(1, self.ranges[-1]))]
+        return [random.randint(x1, x2), random.randint(y1, y2)]
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(rects)
+# param_1 = obj.pick()
