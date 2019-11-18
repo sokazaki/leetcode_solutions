883e17eeb05bbc012337be65a90bd8c81d87701b
sokazaki <okazakisoichiro@gmail.com>
2019-11-18 18:13:32 +0900

    Update 0497_random_point_in_non-overlapping_rectangles.py

--- problems/0497_random_point_in_non-overlapping_rectangles.py
+++ problems/0497_random_point_in_non-overlapping_rectangles.py
@@ -1,12 +1,13 @@
 import random
+import bisect
 
 class Solution:
 
     def __init__(self, rects):
-        self.rects, self.ranges, sm = rects, [], 0
+        self.rects, self.ranges, cum = rects, [], 0
         for x1, y1, x2, y2 in rects:
-            sm += (x2 - x1 + 1) * (y2 - y1 + 1)
-            self.ranges.append(sm)
+            cum += (x2 - x1 + 1) * (y2 - y1 + 1)
+            self.ranges.append(cum)
 
     def pick(self):
         x1, y1, x2, y2 = self.rects[bisect.bisect_left(self.ranges, random.randint(1, self.ranges[-1]))]
