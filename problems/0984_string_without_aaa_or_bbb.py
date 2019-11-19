256af824bc6301280af7335641aae55661ef3d18
sokazaki <okazakisoichiro@gmail.com>
2019-11-19 15:16:39 +0900

    Update 0984_string_without_aaa_or_bbb.py

--- problems/0984_string_without_aaa_or_bbb.py
+++ problems/0984_string_without_aaa_or_bbb.py
@@ -9,10 +9,10 @@ def strWithout3a3b(A, B):
 
     if A >= 2*B:
         return 'aab'* B + 'a'* (A-2*B)
-    elif A >= B:
-        return 'aab' * (A-B) + 'ab' * (2*B - A)
     elif B >= 2*A:
         return 'bba' * A + 'b' *(B-2*A)
+    elif A >= B:
+        return 'aab' * (A-B) + 'ab' * (2*B - A)
     else:
         return 'bba' * (B-A) + 'ab' * (2*A - B)
 
